3c09752d157996e70cbf42a5abd2fd7ceb1d486b
tamnhu11204 <22521053@gm.uit.edu.vn>
2024-05-20 18:17:50 +0700

    demo3

--- FinalGaraOto/NhanVien.xaml.cs
+++ FinalGaraOto/NhanVien.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FinalGaraOto.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,64 @@ namespace FinalGaraOto
         public NhanVien()
         {
             InitializeComponent();
+            LoadNhanVienList();
         }
+        void LoadNhanVienList()
+        {
+            ObservableCollection<NhanViens> nhanViens = new ObservableCollection<NhanViens>();
+            var List=DataProvider.Ins.DB.NGUOIDUNGs.ToList();
+            foreach(var item in List)
+            {
+                NhanViens nhanViens1 = new NhanViens();
+                nhanViens1.Ma = item.MaNguoiDung;
+                nhanViens1.HoVaTen = item.TenNguoiDung;
+                nhanViens1.ChucVu= DataProvider.Ins.DB.NHOMNGUOIDUNGs.Where(x => x.MaNhom == item.MaNhom).Select(x => x.TenNhom).First().ToString();
+                nhanViens.Add(nhanViens1);
+                dtgNhanVien.ItemsSource = nhanViens;
+            }
+           
+        }
+
+        private void BtnClosing_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult r = MessageBox.Show("Bạn có muốn đóng chương trình không?", "Thông báo", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (r == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Btn_ThemNhanVien(object sender, RoutedEventArgs e)
+        {
+            ThemNhanVien themNhanVien = new ThemNhanVien();
+            themNhanVien.ShowDialog();
+        }
+    }
+    public class NhanViens
+    {
+        public int Ma {  get; set; }
+        public string HoVaTen { get; set; }
+        public string ChucVu { get; set; }
     }
 }
--- FinalGaraOto/ThemNhanVien.xaml.cs
+++ FinalGaraOto/ThemNhanVien.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinalGaraOto.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,75 @@ namespace FinalGaraOto
         public ThemNhanVien()
         {
             InitializeComponent();
+            LoadComboBoxChucVu();
+        }
+        void LoadComboBoxChucVu()
+        {
+            var List = DataProvider.Ins.DB.NHOMNGUOIDUNGs.Select(x=>x.TenNhom).ToList();
+            foreach (var item in List)
+            {
+                cbbChucVu.Items.Add(item);
+            }
+        }
+        private void BtnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            NhanVien nhanVien = new NhanVien();
+            nhanVien.Show();
+            this.Close();
+        }
+
+        private void BtnThem_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txbCCCD.Text) || string.IsNullOrEmpty(txbDiaChi.Text) || string.IsNullOrEmpty(txbHoVaTen.Text)
+                || string.IsNullOrEmpty(txbMatKhau.Text) || string.IsNullOrEmpty(txbNgaySinh.Text) || string.IsNullOrEmpty(txbSDT.Text)
+                || string.IsNullOrEmpty(txbTenDangNhap.Text) || string.IsNullOrEmpty(cbbChucVu.Text))
+            {
+                MessageBox.Show("Hãy điền đầy đủ thông tin");
+            }
+            else
+            {
+                string _TenDangNhap = txbTenDangNhap.Text;
+                var TaiKhoan = DataProvider.Ins.DB.NGUOIDUNGs.Where(x => x.TenDangNhap == _TenDangNhap).Count();
+                if(TaiKhoan > 0)
+                {
+                    MessageBox.Show("Tên đăng nhập đã tồn tại! Hãy dùng tên khác.");
+                }
+                else
+                {
+                    NGUOIDUNG n = new NGUOIDUNG();
+                    n.TenNguoiDung = txbHoVaTen.ToString();
+
+                    DateTime? ngaySinh = txbNgaySinh.SelectedDate;
+                    if (ngaySinh.HasValue)
+                    {
+
+                        n.NgaySinhNguoiDung = ngaySinh.Value;
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+                    n.CCCDNguoiDung = txbCCCD.ToString();
+                    n.DiaChiNguoiDung = txbDiaChi.ToString();
+                    n.SDTNguoiDung = txbSDT.ToString();
+                    n.TenDangNhap = txbTenDangNhap.ToString();
+                    n.MatKhau = txbMatKhau.ToString();
+
+                    string selectedValue = cbbChucVu.SelectedItem as string;
+                    var l = DataProvider.Ins.DB.NHOMNGUOIDUNGs.Where(x => x.TenNhom == selectedValue).First();
+                    n.MaNhom = l.MaNhom;
+
+                    using (var dbContext = new QLGARAOTOEntities()) 
+                    {
+                        dbContext.NGUOIDUNGs.Add(n);
+                    }
+
+                    MessageBox.Show("Thêm nhân viên thành công!");
+                    this.Close();
+                }
+            }
+            return;
         }
     }
 }
